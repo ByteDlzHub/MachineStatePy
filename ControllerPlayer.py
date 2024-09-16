add440fe0d03c1e00bf57508986a2229538c0a2c
ByteDlzHub <bytedlzhub@gmail.com>
2024-09-16 17:37:12 -0400

    First Commit

---
+++ ControllerPlayer.py
@@ -0,0 +1,16 @@
+import keyboard
+
+
+print("Press any key to continue...")
+key = keyboard.read_key()
+print(f"You pressed {key}\r")
+
+
+#Modules
+
+#Player
+class Player:
+    def __init__(self) -> None:
+        pass
+
+#Movement / States
